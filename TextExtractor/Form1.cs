37625fc94ee8106e7661d7cf2ec5cc2d567fb075
KuluK-Max <maksnm748@gmail.com>
2023-09-15 17:26:19 +0600

    Add packages to parse data

--- TextExtractor/Form1.cs
+++ TextExtractor/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace TextExtractor
 {
     public partial class Form1 : Form
@@ -16,5 +17,7 @@ namespace TextExtractor
         {
             InitializeComponent();
         }
+
+
     }
 }
